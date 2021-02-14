9b4393992f0fa049b16db81f90a7fd832cc77c89
vvkt453 <vivek@trendlyne.com>
2021-02-14 10:52:03 +0530

    initial commit, burger ingredients and build controls added

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array((props.ingredients[igKey]))].map((_,i) => {
+                // console.log(igKey);
+                return <BurgerIngredient key={igKey+i} type={igKey} />
+            })
+        })
+
+    console.log(transformedIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    )
+}
+
+export default burger;
